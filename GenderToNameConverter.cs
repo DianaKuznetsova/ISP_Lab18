b09b2c88e8e6c34eea4bf41606fa3ec08e3cab1b
DianaKuznetsova <dianakyznetsova@yandex.by>
2020-12-05 19:30:26 +0300

    UI updated
    Gender convereter added

---
+++ GenderToNameConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ISP_Lab18
+{
+    class GenderToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((Gender)value) {
+                case Gender.Female: return "Женский";
+                case Gender.Male: return "Мужской";
+                default: throw new ArgumentException("Unsupported gender: " + value);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
